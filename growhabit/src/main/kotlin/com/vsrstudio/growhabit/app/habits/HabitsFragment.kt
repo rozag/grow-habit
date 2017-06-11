c22792b7f4e1edceaed617d2482bc47565786524
Alexey Mileev <alemileev@gmail.com>
2017-06-11 20:03:21 +0300

    Add fragments stubs

--- growhabit/src/main/kotlin/com/vsrstudio/growhabit/app/habits/HabitsFragment.kt
+++ growhabit/src/main/kotlin/com/vsrstudio/growhabit/app/habits/HabitsFragment.kt
@@ -1,11 +1,22 @@
 package com.vsrstudio.growhabit.app.habits
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vsrstudio.growhabit.R
 import com.vsrstudio.growhabit.arch.ArchBaseFragment
 
 class HabitsFragment : ArchBaseFragment<HabitsController, HabitsReducer, HabitsViewState>() {
 
     override val controller: HabitsController = HabitsController()
     override val reducer: HabitsReducer = HabitsReducer(this)
+
+    override fun onCreateView(inflater: LayoutInflater?,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_habits, container, false)
+    }
 
     override fun updateViewState(viewState: HabitsViewState) {
         // TODO:
