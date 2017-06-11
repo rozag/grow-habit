fd8fadec400f3a9306c96d7478d98b7a6a11357d
Alexey Mileev <alemileev@gmail.com>
2017-06-11 19:58:46 +0300

    Add bottom navigation view

--- growhabit/src/main/kotlin/com/vsrstudio/growhabit/app/sections/SectionsActivity.kt
+++ growhabit/src/main/kotlin/com/vsrstudio/growhabit/app/sections/SectionsActivity.kt
@@ -3,10 +3,17 @@ package com.vsrstudio.growhabit.app.sections
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.view.MenuItem
 import com.vsrstudio.growhabit.R
+import com.vsrstudio.growhabit.app.assistant.AssistantFragment
+import com.vsrstudio.growhabit.app.habits.HabitsFragment
+import com.vsrstudio.growhabit.app.statistics.StatisticsFragment
 import com.vsrstudio.growhabit.arch.ArchBaseActivity
 
-class SectionsActivity : ArchBaseActivity<SectionsController, SectionsReducer, SectionsViewState>() {
+class SectionsActivity : ArchBaseActivity<SectionsController, SectionsReducer, SectionsViewState>(),
+        BottomNavigationView.OnNavigationItemSelectedListener {
 
     companion object {
         fun makeIntent(context: Context): Intent {
@@ -23,13 +30,54 @@ class SectionsActivity : ArchBaseActivity<SectionsController, SectionsReducer, S
     override val controller: SectionsController = SectionsController()
     override val reducer: SectionsReducer = SectionsReducer(this, controller)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // TODO: select position 1 tab by default
+    private val bottomNavigationView by lazy {
+        findViewById(R.id.sections_bottom_navigation_view) as BottomNavigationView
     }
 
-    override fun updateViewState(viewState: SectionsViewState) {
-        // TODO:
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        bottomNavigationView.selectedItemId = R.id.action_habits
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.action_assistant -> {
+            showAssistant()
+            true
+        }
+        R.id.action_habits -> {
+            showHabits()
+            true
+        }
+        R.id.action_statistics -> {
+            showStatistics()
+            true
+        }
+        else -> false
+    }
+
+    override fun updateViewState(viewState: SectionsViewState) = when (viewState) {
+        SectionsViewState.AssistantState -> showAssistant()
+        SectionsViewState.HabitsState -> showHabits()
+        SectionsViewState.StatisticsState -> showStatistics()
+    }
+
+    private fun showAssistant() {
+        showFragment(AssistantFragment())
+    }
+
+    private fun showHabits() {
+        showFragment(HabitsFragment())
+    }
+
+    private fun showStatistics() {
+        showFragment(StatisticsFragment())
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.sections_content, fragment)
+                .commit()
     }
 
 }
