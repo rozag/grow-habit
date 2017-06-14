c1ae4791ad82ffcdc320ed013f23aa9161ea2f5b
Alexey Mileev <alemileev@gmail.com>
2017-06-15 01:38:45 +0300

    Remove useless comletion repo

--- growhabit/src/main/kotlin/com/vsrstudio/growhabit/repo/completion/CompletionRepo.kt
+++
@@ -1,7 +0,0 @@
-package com.vsrstudio.growhabit.repo.completion
-
-import com.vsrstudio.growhabit.model.Completion
-import com.vsrstudio.growhabit.repo.Repo
-import com.vsrstudio.growhabit.repo.Spec
-
-interface CompletionRepo<in S : Spec> : Repo<Completion, S>
