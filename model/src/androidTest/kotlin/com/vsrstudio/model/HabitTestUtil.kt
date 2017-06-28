d61219832ad7d0b1362b0f9fd39686ec2fe72ec1
Alexey Mileev <alemileev@gmail.com>
2017-06-29 02:13:48 +0300

    Extract habit test util file

---
+++ model/src/androidTest/kotlin/com/vsrstudio/model/HabitTestUtil.kt
@@ -0,0 +1,228 @@
+package com.vsrstudio.model
+
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import com.vsrstudio.arch.Query
+import com.vsrstudio.entity.domain.*
+import com.vsrstudio.model.HabitsSqliteOpenHelper.Scheme.CompletionEntry
+import com.vsrstudio.model.HabitsSqliteOpenHelper.Scheme.HabitEntry
+import com.vsrstudio.model.HabitsSqliteOpenHelper.Scheme.Table
+import com.vsrstudio.model.mapper.CompletionFromCursorMapper
+import com.vsrstudio.model.mapper.CompletionToContentValuesMapper
+import com.vsrstudio.model.mapper.HabitFromCursorMapper
+import com.vsrstudio.model.mapper.HabitToContentValuesMapper
+import io.reactivex.observers.TestObserver
+
+fun applyToWritableDb(writableDb: SQLiteDatabase, func: (SQLiteDatabase) -> Unit) {
+    writableDb.beginTransaction()
+    func(writableDb)
+    writableDb.setTransactionSuccessful()
+    writableDb.endTransaction()
+}
+
+fun addHabits(writableDb: SQLiteDatabase,
+              habits: List<Habit>,
+              habitToContentValuesMapper: HabitToContentValuesMapper,
+              completionToContentValuesMapper: CompletionToContentValuesMapper) {
+    applyToWritableDb(writableDb) { writableDatabase ->
+        val habitsContentValues = habitToContentValuesMapper.batchMap(habits)
+        val completionsContentValues = habits.flatMap { habit ->
+            completionToContentValuesMapper.batchMap(habit.completions)
+        }
+        habitsContentValues.forEach { cv ->
+            writableDatabase.insert(Table.habit, null, cv)
+        }
+        completionsContentValues.forEach { cv ->
+            writableDatabase.insert(Table.completion, null, cv)
+        }
+    }
+}
+
+fun queryAllHabits(readableDb: SQLiteDatabase,
+                   completionFromCursorMapper: CompletionFromCursorMapper,
+                   habitFromCursorMapper: HabitFromCursorMapper): List<Habit> {
+    val habitsCursor = readableDb.rawQuery(
+            "SELECT * FROM ${Table.habit};",
+            null
+    )
+    val habitsIdsSelection = buildHabitsIdsSelectionString(habitsCursor)
+    val completionsCursor = readableDb.rawQuery(
+            "SELECT * " +
+                    "FROM ${Table.completion} " +
+                    "WHERE ${CompletionEntry.habitId} IN ($habitsIdsSelection);",
+            null
+    )
+    val completions = completionFromCursorMapper.batchMap(completionsCursor)
+    completionsCursor.close()
+    val habitIdToCompletionListMap = buildHabitIdToCompletionsListMapping(completions)
+    val habits = habitFromCursorMapper.batchMap(habitsCursor, habitIdToCompletionListMap)
+    habitsCursor.close()
+    return habits
+}
+
+fun buildHabitsIdsSelectionString(habitsCursor: Cursor): String {
+    val habitsIdsList = ArrayList<String>(habitsCursor.count)
+    val idColInd = habitsCursor.getColumnIndex(HabitEntry.id)
+    habitsCursor.moveToFirst()
+    val habitsIdsSelection: String
+    if (habitsCursor.count > 0) {
+        do {
+            habitsIdsList.add(habitsCursor.getString(idColInd))
+        } while (habitsCursor.moveToNext())
+        val sb = StringBuilder()
+        habitsIdsList.forEachIndexed { index, id ->
+            sb.append("'$id'")
+            if (index != habitsIdsList.lastIndex) {
+                sb.append(", ")
+            }
+        }
+        habitsIdsSelection = sb.toString()
+    } else {
+        habitsIdsSelection = ""
+    }
+    return habitsIdsSelection
+}
+
+fun buildHabitIdToCompletionsListMapping(completions: List<Completion>):
+        MutableMap<String, MutableList<Completion>> {
+    val habitIdToCompletionListMap = mutableMapOf<String, MutableList<Completion>>()
+    completions.forEach { completion ->
+        val habitId = completion.habitId.value
+        val nullableListFromMap: MutableList<Completion>? = habitIdToCompletionListMap[habitId]
+        val listFromMap: MutableList<Completion>
+        if (nullableListFromMap == null) {
+            listFromMap = ArrayList<Completion>()
+            habitIdToCompletionListMap[habitId] = listFromMap
+        } else {
+            listFromMap = nullableListFromMap
+        }
+        listFromMap.add(completion)
+    }
+    return habitIdToCompletionListMap
+}
+
+fun generateHabitId(index: Int): Id = Id("habit_id_$index")
+
+fun generateHabitTitle(index: Int): Title = Title("habit_title_$index")
+
+fun generateCompletionId(index: Int, postfix: Int): Id = Id("completion_id_${postfix}_$index")
+
+fun randomLongFromInterval(begin: Long, end: Long): Long {
+    val range = end - begin + 1
+    return (Math.random() * range).toLong() + begin
+}
+
+fun generateCompletion(id: Id, habitId: Id): Completion {
+    return Completion(
+            id,
+            habitId,
+            Completion.Status.fromInt(randomLongFromInterval(0, 3).toInt()),
+            Date(randomLongFromInterval(1, 1000))
+    )
+}
+
+fun generateCompletionsList(habitId: Id, index: Int, count: Int = 3): List<Completion> {
+    return (0..count - 1)
+            .map { i -> generateCompletionId(i, index) }
+            .map { completionId -> generateCompletion(completionId, habitId) }
+}
+
+fun generateHabit(index: Int = 0): Habit {
+    val habitId = generateHabitId(index)
+    val habitTitle = generateHabitTitle(index)
+    val completions = generateCompletionsList(habitId, index)
+    return Habit(habitId, habitTitle, completions, index)
+}
+
+fun generateHabitsList(count: Int = 3): List<Habit> {
+    return (0..count - 1)
+            .map { index -> generateHabit(index) }
+}
+
+fun generateUpdatedHabit(habit: Habit, newIndex: Int): Habit {
+    val newTitle = generateHabitTitle(newIndex)
+    val newPosition = habit.position + 1
+    val newCompletionId = generateCompletionId(habit.completions.size, newIndex)
+    val newCompletion = generateCompletion(newCompletionId, habit.id)
+    return habit.rename(newTitle)
+            .addCompletion(newCompletion)
+            .changePosition(newPosition)
+}
+
+fun generateUpdatedHabitsList(habits: List<Habit>): List<Habit> {
+    return habits.mapIndexed { index, habit -> generateUpdatedHabit(habit, habits.size + index) }
+}
+
+fun generateQueryForSingleHabit(id: Id,
+                                completionFromCursorMapper: CompletionFromCursorMapper,
+                                habitFromCursorMapper: HabitFromCursorMapper): Query<Habit, SQLiteDatabase> {
+    return object : Query<Habit, SQLiteDatabase> {
+        override fun query(readableStorage: SQLiteDatabase): List<Habit> {
+            return queryAllHabits(readableStorage, completionFromCursorMapper, habitFromCursorMapper)
+                    .filter { (habitId) -> habitId == id }
+        }
+
+        override fun uniqueId(): String = "query_single_habits"
+    }
+}
+
+fun generateQueryForSeveralHabits(ids: List<Id>,
+                                  completionFromCursorMapper: CompletionFromCursorMapper,
+                                  habitFromCursorMapper: HabitFromCursorMapper): Query<Habit, SQLiteDatabase> {
+    return object : Query<Habit, SQLiteDatabase> {
+        override fun query(readableStorage: SQLiteDatabase): List<Habit> {
+            return queryAllHabits(readableStorage, completionFromCursorMapper, habitFromCursorMapper)
+                    .filter { (habitId) -> habitId in ids }
+        }
+
+        override fun uniqueId(): String = "query_several_habits"
+    }
+}
+
+fun generateQueryForAllHabits(completionFromCursorMapper: CompletionFromCursorMapper,
+                              habitFromCursorMapper: HabitFromCursorMapper): Query<Habit, SQLiteDatabase> {
+    return object : Query<Habit, SQLiteDatabase> {
+        override fun query(readableStorage: SQLiteDatabase): List<Habit> {
+            return queryAllHabits(readableStorage, completionFromCursorMapper, habitFromCursorMapper)
+        }
+
+        override fun uniqueId(): String = "query_all_habits"
+    }
+}
+
+fun subscribeDifferentObservers(repo: HabitsSqliteRepo,
+                                habits: List<Habit>,
+                                completionFromCursorMapper: CompletionFromCursorMapper,
+                                habitFromCursorMapper: HabitFromCursorMapper): List<TestObserver<List<Habit>>> {
+    return listOf(
+            repo.query(generateQueryForSingleHabit(
+                    habits[0].id,
+                    completionFromCursorMapper,
+                    habitFromCursorMapper
+            ))
+                    .skip(1) // Skip the first emission
+                    .test(),
+            repo.query(generateQueryForSeveralHabits(
+                    habits.filterIndexed { index, _ -> index % 2 == 0 }.map { (id) -> id },
+                    completionFromCursorMapper,
+                    habitFromCursorMapper
+            ))
+                    .skip(1) // Skip the first emission
+                    .test(),
+            repo.query(generateQueryForAllHabits(completionFromCursorMapper, habitFromCursorMapper))
+                    .skip(1) // Skip the first emission
+                    .test()
+    )
+}
+
+fun assertObserversValues(observers: List<TestObserver<List<Habit>>>, habits: List<Habit>) {
+    observers[0].assertValue(
+            if (habits.isEmpty()) {
+                habits
+            } else {
+                habits.subList(0, 1)
+            }
+    )
+    observers[1].assertValue(habits.filterIndexed { index, _ -> index % 2 == 0 })
+    observers[2].assertValue(habits)
+}
--- model/src/androidTest/kotlin/com/vsrstudio/model/HabitsSqliteRepoTest.kt
+++ model/src/androidTest/kotlin/com/vsrstudio/model/HabitsSqliteRepoTest.kt
@@ -1,20 +1,15 @@
 package com.vsrstudio.model
 
 import android.content.Context
-import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.support.test.InstrumentationRegistry
 import com.vsrstudio.arch.Query
-import com.vsrstudio.entity.domain.*
-import com.vsrstudio.model.HabitsSqliteOpenHelper.Scheme.CompletionEntry
+import com.vsrstudio.entity.domain.Habit
 import com.vsrstudio.model.HabitsSqliteOpenHelper.Scheme.DatabaseInfo
-import com.vsrstudio.model.HabitsSqliteOpenHelper.Scheme.HabitEntry
-import com.vsrstudio.model.HabitsSqliteOpenHelper.Scheme.Table
 import com.vsrstudio.model.mapper.CompletionFromCursorMapper
 import com.vsrstudio.model.mapper.CompletionToContentValuesMapper
 import com.vsrstudio.model.mapper.HabitFromCursorMapper
 import com.vsrstudio.model.mapper.HabitToContentValuesMapper
-import io.reactivex.observers.TestObserver
 import junit.framework.Assert.assertEquals
 import org.junit.After
 import org.junit.Before
@@ -54,50 +49,109 @@ class HabitsSqliteRepoTest {
     fun addSingleHabit_habitAdded() {
         val habitToAdd = generateHabit()
         repo.add(habitToAdd)
-        assertEquals(listOf(habitToAdd), queryAllHabits(dbOpenHelper.readableDatabase))
+        assertEquals(
+                listOf(habitToAdd),
+                queryAllHabits(
+                        dbOpenHelper.readableDatabase,
+                        completionFromCursorMapper,
+                        habitFromCursorMapper
+                )
+        )
     }
 
     @Test
     fun addMultipleHabits_habitsAdded() {
         val habitsList = generateHabitsList()
         repo.add(habitsList)
-        assertEquals(habitsList, queryAllHabits(dbOpenHelper.readableDatabase))
+        assertEquals(
+                habitsList,
+                queryAllHabits(
+                        dbOpenHelper.readableDatabase,
+                        completionFromCursorMapper,
+                        habitFromCursorMapper
+                )
+        )
     }
 
     @Test
     fun updateSingleHabit_habitUpdated() {
         val oldIndex = 0
         val habit = generateHabit(oldIndex)
-        addHabits(dbOpenHelper.writableDatabase, listOf(habit))
+        addHabits(
+                dbOpenHelper.writableDatabase,
+                listOf(habit),
+                habitToContentValuesMapper,
+                completionToContentValuesMapper
+        )
         val updatedHabit = generateUpdatedHabit(habit, oldIndex)
         repo.update(updatedHabit)
-        assertEquals(listOf(updatedHabit), queryAllHabits(dbOpenHelper.readableDatabase))
+        assertEquals(
+                listOf(updatedHabit),
+                queryAllHabits(
+                        dbOpenHelper.readableDatabase,
+                        completionFromCursorMapper,
+                        habitFromCursorMapper
+                )
+        )
     }
 
     @Test
     fun updateMultipleHabits_habitsUpdated() {
         val habits = generateHabitsList()
-        addHabits(dbOpenHelper.writableDatabase, habits)
+        addHabits(
+                dbOpenHelper.writableDatabase,
+                habits,
+                habitToContentValuesMapper,
+                completionToContentValuesMapper
+        )
         val updatedHabits = generateUpdatedHabitsList(habits)
         repo.update(updatedHabits)
-        assertEquals(updatedHabits, queryAllHabits(dbOpenHelper.readableDatabase))
+        assertEquals(
+                updatedHabits,
+                queryAllHabits(
+                        dbOpenHelper.readableDatabase,
+                        completionFromCursorMapper,
+                        habitFromCursorMapper
+                )
+        )
     }
 
     @Test
     fun removeSingleHabit_habitRemoved() {
         val habit = generateHabit()
-        addHabits(dbOpenHelper.writableDatabase, listOf(habit))
+        addHabits(
+                dbOpenHelper.writableDatabase,
+                listOf(habit),
+                habitToContentValuesMapper,
+                completionToContentValuesMapper
+        )
         repo.remove(habit)
-        assertEquals(listOf<Habit>(), queryAllHabits(dbOpenHelper.readableDatabase))
+        assertEquals(
+                listOf<Habit>(),
+                queryAllHabits(
+                        dbOpenHelper.readableDatabase,
+                        completionFromCursorMapper,
+                        habitFromCursorMapper
+                )
+        )
     }
 
     @Test
     fun queryAllHabits_allHabitsReturned() {
         val habits = generateHabitsList()
-        addHabits(dbOpenHelper.writableDatabase, habits)
+        addHabits(
+                dbOpenHelper.writableDatabase,
+                habits,
+                habitToContentValuesMapper,
+                completionToContentValuesMapper
+        )
         val allHabitsQuery = object : Query<Habit, SQLiteDatabase> {
             override fun query(readableStorage: SQLiteDatabase): List<Habit> {
-                return queryAllHabits(dbOpenHelper.writableDatabase)
+                return queryAllHabits(
+                        dbOpenHelper.writableDatabase,
+                        completionFromCursorMapper,
+                        habitFromCursorMapper
+                )
             }
 
             override fun uniqueId(): String = "test_query_id"
@@ -111,7 +165,12 @@ class HabitsSqliteRepoTest {
     fun addSingleHabit_observersNotified() {
         val habitToAdd = generateHabit()
         val habitsList = listOf(habitToAdd)
-        val observers = subscribeDifferentObservers(repo, habitsList)
+        val observers = subscribeDifferentObservers(
+                repo,
+                habitsList,
+                completionFromCursorMapper,
+                habitFromCursorMapper
+        )
         repo.add(habitToAdd)
         assertObserversValues(observers, habitsList)
     }
@@ -119,7 +178,12 @@ class HabitsSqliteRepoTest {
     @Test
     fun addMultipleHabits_observersNotified() {
         val habitsList = generateHabitsList()
-        val observers = subscribeDifferentObservers(repo, habitsList)
+        val observers = subscribeDifferentObservers(
+                repo,
+                habitsList,
+                completionFromCursorMapper,
+                habitFromCursorMapper
+        )
         repo.add(habitsList)
         assertObserversValues(observers, habitsList)
     }
@@ -128,10 +192,20 @@ class HabitsSqliteRepoTest {
     fun updateSingleHabit_observersNotified() {
         val oldIndex = 0
         val habit = generateHabit(oldIndex)
-        addHabits(dbOpenHelper.writableDatabase, listOf(habit))
+        addHabits(
+                dbOpenHelper.writableDatabase,
+                listOf(habit),
+                habitToContentValuesMapper,
+                completionToContentValuesMapper
+        )
         val updatedHabit = generateUpdatedHabit(habit, oldIndex)
         val habits = listOf(updatedHabit)
-        val observers = subscribeDifferentObservers(repo, habits)
+        val observers = subscribeDifferentObservers(
+                repo,
+                habits,
+                completionFromCursorMapper,
+                habitFromCursorMapper
+        )
         repo.update(updatedHabit)
         assertObserversValues(observers, habits)
     }
@@ -139,9 +213,19 @@ class HabitsSqliteRepoTest {
     @Test
     fun updateMultipleHabits_observersNotified() {
         val habits = generateHabitsList()
-        addHabits(dbOpenHelper.writableDatabase, habits)
+        addHabits(
+                dbOpenHelper.writableDatabase,
+                habits,
+                habitToContentValuesMapper,
+                completionToContentValuesMapper
+        )
         val updatedHabits = generateUpdatedHabitsList(habits)
-        val observers = subscribeDifferentObservers(repo, updatedHabits)
+        val observers = subscribeDifferentObservers(
+                repo,
+                updatedHabits,
+                completionFromCursorMapper,
+                habitFromCursorMapper
+        )
         repo.update(updatedHabits)
         assertObserversValues(observers, updatedHabits)
     }
@@ -150,205 +234,20 @@ class HabitsSqliteRepoTest {
     fun removeSingleHabit_observersNotified() {
         val habit = generateHabit()
         val habitList = listOf(habit)
-        addHabits(dbOpenHelper.writableDatabase, habitList)
-        val observers = subscribeDifferentObservers(repo, habitList)
+        addHabits(
+                dbOpenHelper.writableDatabase,
+                habitList,
+                habitToContentValuesMapper,
+                completionToContentValuesMapper
+        )
+        val observers = subscribeDifferentObservers(
+                repo,
+                habitList,
+                completionFromCursorMapper,
+                habitFromCursorMapper
+        )
         repo.remove(habit)
         assertObserversValues(observers, listOf())
-    }
-
-    private fun applyToWritableDb(writableDb: SQLiteDatabase, func: (SQLiteDatabase) -> Unit) {
-        writableDb.beginTransaction()
-        func(writableDb)
-        writableDb.setTransactionSuccessful()
-        writableDb.endTransaction()
-    }
-
-    private fun addHabits(writableDb: SQLiteDatabase, habits: List<Habit>) {
-        applyToWritableDb(writableDb) { writableDatabase ->
-            val habitsContentValues = habitToContentValuesMapper.batchMap(habits)
-            val completionsContentValues = habits.flatMap { habit ->
-                completionToContentValuesMapper.batchMap(habit.completions)
-            }
-            habitsContentValues.forEach { cv ->
-                writableDatabase.insert(Table.habit, null, cv)
-            }
-            completionsContentValues.forEach { cv ->
-                writableDatabase.insert(Table.completion, null, cv)
-            }
-        }
-    }
-
-    private fun queryAllHabits(readableDb: SQLiteDatabase): List<Habit> {
-        val habitsCursor = readableDb.rawQuery(
-                "SELECT * FROM ${Table.habit};",
-                null
-        )
-        val habitsIdsSelection = buildHabitsIdsSelectionString(habitsCursor)
-        val completionsCursor = readableDb.rawQuery(
-                "SELECT * " +
-                        "FROM ${Table.completion} " +
-                        "WHERE ${CompletionEntry.habitId} IN ($habitsIdsSelection);",
-                null
-        )
-        val completions = completionFromCursorMapper.batchMap(completionsCursor)
-        completionsCursor.close()
-        val habitIdToCompletionListMap = buildHabitIdToCompletionsListMapping(completions)
-        val habits = habitFromCursorMapper.batchMap(habitsCursor, habitIdToCompletionListMap)
-        habitsCursor.close()
-        return habits
-    }
-
-    private fun buildHabitsIdsSelectionString(habitsCursor: Cursor): String {
-        val habitsIdsList = ArrayList<String>(habitsCursor.count)
-        val idColInd = habitsCursor.getColumnIndex(HabitEntry.id)
-        habitsCursor.moveToFirst()
-        val habitsIdsSelection: String
-        if (habitsCursor.count > 0) {
-            do {
-                habitsIdsList.add(habitsCursor.getString(idColInd))
-            } while (habitsCursor.moveToNext())
-            val sb = StringBuilder()
-            habitsIdsList.forEachIndexed { index, id ->
-                sb.append("'$id'")
-                if (index != habitsIdsList.lastIndex) {
-                    sb.append(", ")
-                }
-            }
-            habitsIdsSelection = sb.toString()
-        } else {
-            habitsIdsSelection = ""
-        }
-        return habitsIdsSelection
-    }
-
-    private fun buildHabitIdToCompletionsListMapping(completions: List<Completion>):
-            MutableMap<String, MutableList<Completion>> {
-        val habitIdToCompletionListMap = mutableMapOf<String, MutableList<Completion>>()
-        completions.forEach { completion ->
-            val habitId = completion.habitId.value
-            val nullableListFromMap: MutableList<Completion>? = habitIdToCompletionListMap[habitId]
-            val listFromMap: MutableList<Completion>
-            if (nullableListFromMap == null) {
-                listFromMap = ArrayList<Completion>()
-                habitIdToCompletionListMap[habitId] = listFromMap
-            } else {
-                listFromMap = nullableListFromMap
-            }
-            listFromMap.add(completion)
-        }
-        return habitIdToCompletionListMap
-    }
-
-    private fun generateHabitId(index: Int): Id = Id("habit_id_$index")
-
-    private fun generateHabitTitle(index: Int): Title = Title("habit_title_$index")
-
-    private fun generateCompletionId(index: Int, postfix: Int): Id = Id("completion_id_${postfix}_$index")
-
-    private fun randomLongFromInterval(begin: Long, end: Long): Long {
-        val range = end - begin + 1
-        return (Math.random() * range).toLong() + begin
-    }
-
-    private fun generateCompletion(id: Id, habitId: Id): Completion {
-        return Completion(
-                id,
-                habitId,
-                Completion.Status.fromInt(randomLongFromInterval(0, 3).toInt()),
-                Date(randomLongFromInterval(1, 1000))
-        )
-    }
-
-    private fun generateCompletionsList(habitId: Id, index: Int, count: Int = 3): List<Completion> {
-        return (0..count - 1)
-                .map { i -> generateCompletionId(i, index) }
-                .map { completionId -> generateCompletion(completionId, habitId) }
-    }
-
-    private fun generateHabit(index: Int = 0): Habit {
-        val habitId = generateHabitId(index)
-        val habitTitle = generateHabitTitle(index)
-        val completions = generateCompletionsList(habitId, index)
-        return Habit(habitId, habitTitle, completions, index)
-    }
-
-    private fun generateHabitsList(count: Int = 3): List<Habit> {
-        return (0..count - 1)
-                .map { index -> generateHabit(index) }
-    }
-
-    private fun generateUpdatedHabit(habit: Habit, newIndex: Int): Habit {
-        val newTitle = generateHabitTitle(newIndex)
-        val newPosition = habit.position + 1
-        val newCompletionId = generateCompletionId(habit.completions.size, newIndex)
-        val newCompletion = generateCompletion(newCompletionId, habit.id)
-        return habit.rename(newTitle)
-                .addCompletion(newCompletion)
-                .changePosition(newPosition)
-    }
-
-    private fun generateUpdatedHabitsList(habits: List<Habit>): List<Habit> {
-        return habits.mapIndexed { index, habit -> generateUpdatedHabit(habit, habits.size + index) }
-    }
-
-    private fun generateQueryForSingleHabit(id: Id): Query<Habit, SQLiteDatabase> {
-        return object : Query<Habit, SQLiteDatabase> {
-            override fun query(readableStorage: SQLiteDatabase): List<Habit> {
-                return queryAllHabits(readableStorage)
-                        .filter { (habitId) -> habitId == id }
-            }
-
-            override fun uniqueId(): String = "query_single_habits"
-        }
-    }
-
-    private fun generateQueryForSeveralHabits(ids: List<Id>): Query<Habit, SQLiteDatabase> {
-        return object : Query<Habit, SQLiteDatabase> {
-            override fun query(readableStorage: SQLiteDatabase): List<Habit> {
-                return queryAllHabits(readableStorage)
-                        .filter { (habitId) -> habitId in ids }
-            }
-
-            override fun uniqueId(): String = "query_several_habits"
-        }
-    }
-
-    private fun generateQueryForAllHabits(): Query<Habit, SQLiteDatabase> {
-        return object : Query<Habit, SQLiteDatabase> {
-            override fun query(readableStorage: SQLiteDatabase): List<Habit> {
-                return queryAllHabits(readableStorage)
-            }
-
-            override fun uniqueId(): String = "query_all_habits"
-        }
-    }
-
-    private fun subscribeDifferentObservers(repo: HabitsSqliteRepo,
-                                            habits: List<Habit>): List<TestObserver<List<Habit>>> {
-        return listOf(
-                repo.query(generateQueryForSingleHabit(habits[0].id))
-                        .skip(1) // Skip the first emission
-                        .test(),
-                repo.query(generateQueryForSeveralHabits(
-                        habits.filterIndexed { index, _ -> index % 2 == 0 }.map { (id) -> id }))
-                        .skip(1) // Skip the first emission
-                        .test(),
-                repo.query(generateQueryForAllHabits())
-                        .skip(1) // Skip the first emission
-                        .test()
-        )
-    }
-
-    private fun assertObserversValues(observers: List<TestObserver<List<Habit>>>, habits: List<Habit>) {
-        observers[0].assertValue(
-                if (habits.isEmpty()) {
-                    habits
-                } else {
-                    habits.subList(0, 1)
-                }
-        )
-        observers[1].assertValue(habits.filterIndexed { index, _ -> index % 2 == 0 })
-        observers[2].assertValue(habits)
     }
 
 }
